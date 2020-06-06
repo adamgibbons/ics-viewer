477915690cbc631e703ed0200023e668eb298778
Adam Gibbons <adam.d.gibbons@gmail.com>
2020-06-06 17:29:20 -0600

    add prototype

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='row'>
+      <div class='col'>
+        <h3>Input</h3>
+        <p>form goes here</p>
+        <pre>{{vEvent}}</pre>
+      </div>
+      <div class='col'>
+        <h3>Output (vEvent)</h3>
+        <vEventOut v-bind:vEvent="this.vEvent" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vEventOut from './components/vEventOut.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    vEventOut
+  },
+  data() {
+    return {
+      vEvent: {
+        start: [2018, 5, 30, 6, 30],
+        duration: { hours: 6, minutes: 30 },
+        title: 'Bolder Boulder',
+        description: 'Annual 10-kilometer run in Boulder, Colorado',
+        location: 'Folsom Field, University of Colorado (finish line)',
+        url: 'http://www.bolderboulder.com/',
+        geo: { lat: 40.0095, lon: 105.2669 },
+        categories: ['10k races', 'Memorial Day Weekend', 'Boulder CO'],
+        status: 'CONFIRMED',
+        busyStatus: 'BUSY',
+        organizer: { name: 'Admin', email: 'Race@BolderBOULDER.com' },
+        attendees: [
+          { name: 'Adam Gibbons', email: 'adam@example.com', rsvp: true, partstat: 'ACCEPTED', role: 'REQ-PARTICIPANT' },
+          { name: 'Brittany Seaton', email: 'brittany@example2.org', dir: 'https://linkedin.com/in/brittanyseaton', role: 'OPT-PARTICIPANT' }
+        ]
+      }
+    }
   }
 }
 </script>
 
 <style>
+body, html {
+  padding: 0;
+  margin: 0;
+  /*display: flex;*/
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background: #888;
+}
+.row {
+  display: flex;
+  flex-flow: row nowrap;
+  margin: 0;
+  padding: 0;
+}
+.col {
+  /*flex: 1 1 auto;*/
+  background: #ddd;
+  margin: .1em;
+  padding: 1em;
 }
 </style>
---
+++ src/components/vEventOut.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1> -->
+    <!-- <pre>{{vEvent}}</pre> -->
+    <div v-for='(line, index) in output' :key='index'>
+      <div class='line'>{{line}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+const ics = require('ics')
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    vEvent: Object
+  },
+  computed: {
+    output() {
+      return ics.createEvent(this.vEvent, (e, v) => {
+        return v.split('\r\n')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.line {
+  /*border: 1px solid red;*/
+  background: #eee;
+  margin: 4px 0;
+  line-height: 2;
+}
+</style>
